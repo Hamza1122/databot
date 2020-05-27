bb86ec9131db81ffc70c15041eee30894c63c275
Muhammad Hamza <hamzabashir465@gmail.com>
2020-05-27 22:02:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -21,19 +21,7 @@ def tokenize(text):
 
 @app.route('/predict/<name>')
 def result(name):
-    filename = 'knn_bot.pkl'
-    classifier = pickle.load(open(filename, 'rb'))
-    filename1 = 'tfidf.pkl'
-    tfidf_vect = pickle.load(open(filename1,'rb'))
-    df = pd.read_csv('Updated_Dataset.csv')
-    df = df.drop(['Unnamed: 0'], axis=1)
-    user_response = name#"What is a leveraged buyout?"
-
-    test = ["Hello",user_response]
-    tfidf_test = tfidf_vect.transform(test)
-    y_pred = classifier.predict(tfidf_test[1])
-    return df['Answer'][y_pred[0]]
-
+    return "Your name is"+name
 
 
 
