fbbe39d3f0e6de546fde64a8c4769d7a32e4193e
Muhammad Hamza <hamzabashir465@gmail.com>
2020-05-27 21:44:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,54 @@ def predict():
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
+
+@app.route('/home')
+def home_page():
+    return "home Information"
+
+
+@app.route('/course_registraion')
+def course_registration():
+    return "Registraion Information"
+
+
+
+@app.route('/Attendance')
+def attendance():
+    return "Attendance Information"
+
+
+@app.route('/marks')
+def marks():
+    return "marks Information"
+
+
+@app.route('/transcript')
+def transcript():
+    return "Transcript Information"
+
+@app.route('/fee_challan')
+def fee_challan():
+    return "Challan Information"
+
+
+@app.route('/fee_details')
+def fee_details():
+    return "Transcript Information"
+
+
+@app.route('/course_feedback')
+def course_feedback():
+    return "Course feedback Information"
+
+
+@app.route('/tentative_')
+def tentative_studyplan():
+    return "Tentatie Study Plan"
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
