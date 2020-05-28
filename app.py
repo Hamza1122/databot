2048fe25264ec234ec813b62a1e5d4738324a8c3
Muhammad Hamza <hamzabashir465@gmail.com>
2020-05-28 10:06:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+ 
 classifier = KNeighborsClassifier(n_neighbors=1, algorithm = 'brute')
 
 
@@ -21,6 +24,10 @@ def tokenize(text):
 
 def response(question):
     test = ["Hello",question]
+    cv=CountVectorizer()
+    word_count_vector=cv.fit_transform(test)
+    tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+    tfidf_vect=tfidf_transformer.fit(word_count_vector)
     tfidf_test = tfidf_vect.transform(test)
     y_pred = classifier.predict(tfidf_test[1])
     return df['Answer'][y_pred[0]]
@@ -32,7 +39,7 @@ def result():
     filename = 'knn_bot.pkl'
     classifier = pickle.load(open(filename,'rb'))
     filename1 = 'tfidf.pkl'
-    tfidf_vect = pickle.load(open(filename1,'rb'))
+   # tfidf_vect = pickle.load(open(filename1,'rb'))
     df = pd.read_csv('Updated_Dataset.csv')
     df = df.drop(['Unnamed: 0'], axis=1)
     user_response = "What is a leveraged buyout?"
