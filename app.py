21e29ac28485460c33f063f6a7e57f502edb17b6
Muhammad Hamza <hamzabashir465@gmail.com>
2020-05-28 07:57:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,42 +2,40 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import string
-import nltk
 import pandas as pd
+from pattern.en import lemma, lexeme
 from sklearn.neighbors import KNeighborsClassifier
 classifier = KNeighborsClassifier(n_neighbors=1, algorithm = 'brute')
 
 
 app = Flask(__name__)
-
 #PreProcessing
 def tokenize(text):
     text = text.lower()
-    #tokens = nltk.word_tokenize(text)
-    words=text.split()
-    table = str.maketrans('', '', string.punctuation)
-    tokens = [w.translate(table) for w in words]
+    tokens = nltk.word_tokenize(text)
     updated = []
     for item in tokens:
         updated.append(lemma(item))
     return updated
 
+
 @app.route('/predict/<name>')
 def result(name):
-    #filename = 'knn_bot.pkl'
-    #classifier = pickle.load(open(filename, 'rb'))
-    #filename1 = 'tfidf.pkl'
-    #tfidf_vect = pickle.load(open(filename1,'rb'))
-    #df = pd.read_csv('Updated_Dataset.csv')
-    #df = df.drop(['Unnamed: 0'], axis=1)
-    #user_response = name#"What is a leveraged buyout?"
+    
+    filename = 'knn_bot.pkl'
+    classifier = pickle.load(open(filename, 'rb'))
+    filename1 = 'tfidf.pkl'
+    tfidf_vect = pickle.load(open(filename1,'rb'))
+    df = pd.read_csv('Updated_Dataset.csv')
+    df = df.drop(['Unnamed: 0'], axis=1)
+    user_response = name#"What is a leveraged buyout?"
 
-    #test = ["Hello",user_response]
-    #tfidf_test = tfidf_vect.transform(test)
-    #y_pred = classifier.predict(tfidf_test[1])
-    #return df['Answer'][y_pred[0]]
-     return "your name is" +name
+    test = ["Hello",user_response]
+    tfidf_test = tfidf_vect.transform(test)
+    y_pred = classifier.predict(tfidf_test[1])
+    return df['Answer'][y_pred[0]]
 
+    #return "your name is" +name
 
 
 
