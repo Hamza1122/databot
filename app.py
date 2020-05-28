a19ac0e10a8eb115afd8cc28840f92acf8cf9ae9
Muhammad Hamza <hamzabashir465@gmail.com>
2020-05-28 19:03:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -51,19 +51,6 @@ def data1():
     Responses = ["I am Good","Hello","hi", "Good","Very well, thanks.","Thankyou","I am doing well","The sky's up but I'm fine thanks. What about you?"]
     user_response ="User: Is a bussiness plan important?"
     user_response=user_response.lower()
-    if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
-            #print("ROBO: You are welcome..")
-        else:
-            #resp = greeting(user_response)
-            if(resp!=0):
-                #print("ROBO: "+greeting(user_response))
-            else:
-                #print("ROBO: ",end="")
-                #print(response(user_response))
-    else:
-
-      #print("ROBO: Bye! take care..")
     return "chrome driver not working on cloud we'll fix it"
 
 
